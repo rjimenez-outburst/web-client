0d431c898ce94379643c6f1cb59b4ce16ce6ecfa
ricardo <ricardo.jimenez@aplaceformom.com>
2020-08-15 08:40:25 -0600

    Add Initial Config

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import Feature from '@features/feature';
 
 function App() {
+  function hey(msj: any) {
+    return msj;
+  }
+
+  useEffect(() => {
+    hey('holis');
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Feature />
     </div>
   );
 }
---
+++ src/features/feature.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Feature() {
+  return <div>Alias is working!</div>;
+}
+
+export default Feature;
